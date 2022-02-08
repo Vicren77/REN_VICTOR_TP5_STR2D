b13ef5ff95308270388275082d3aee4a4a87d764
Vicren77 <46035206+Vicren77@users.noreply.github.com>
2022-02-08 13:44:46 +0100

    Add files via upload

---
+++ GildedRoseTest/GildedRoseTest/GildedRoseTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace csharp
+{
+    [TestFixture]
+    public class GildedRoseTest
+    {
+        [Test]
+        public void ItemSellinShouldDropByOneEachDay()
+        {
+            var item = new Item { Name = "Normal item", SellIn = 3, Quality = 5 };
+            IList<Item> Items = new List<Item> { item };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(item.SellIn, 2);
+        }
+
+        [Test]
+        public void ItemQualityShouldDropByOneEachDay()
+        {
+            var item = new Item { Name = "Normal item", SellIn = 3, Quality = 5 };
+            IList<Item> Items = new List<Item> { item };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(item.Quality, 4);
+        }
+    }
+}
